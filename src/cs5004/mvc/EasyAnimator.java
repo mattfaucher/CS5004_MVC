a197659d8d3df20e31efebd2a1b719bca1419cd2
Matt Faucher <mfauch4444@gmail.com>
2021-04-22 10:42:10 -0400

    Moved code from main to the controller where it will handle the starting of the program

--- src/cs5004/mvc/EasyAnimator.java
+++ src/cs5004/mvc/EasyAnimator.java
@@ -1,23 +1,19 @@
 package cs5004.mvc;
 
+import cs5004.mvc.controller.Controller;
 import cs5004.mvc.model.IModel;
 import cs5004.mvc.model.ModelImpl;
 import cs5004.mvc.util.AnimationBuilder;
 import cs5004.mvc.util.AnimationBuilderImpl;
 import cs5004.mvc.util.AnimationReader;
-import cs5004.mvc.view.GraphicalView;
 import cs5004.mvc.view.IView;
 import cs5004.mvc.view.TypeOfView;
 import cs5004.mvc.view.ViewFactory;
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import javax.swing.JOptionPane;
-import javax.swing.Timer;
 
 /**
  * Main method for the EasyAnimator, allows command line arguments to dictate the running of the
@@ -37,20 +33,19 @@ public class EasyAnimator {
     String outfile = "";
     TypeOfView type = TypeOfView.TEXT;
     int speed = 1;
-    JOptionPane error = new JOptionPane();
 
-    if (args.length == 0 || args.length < 4) {
+    // need a minimum of 4 arguments to be valid input
+    if (args.length < 4) {
       System.out.println("Incorrect arguments passed");
       System.exit(0);
     }
 
+    // Parse command line arguments
     for (String s : args) {
       if (s.equals("-in")) {
         // out file is the next arg
         infile = args[Arrays.asList(args).indexOf(s) + 1];
         if (infile.equals("") || infile == null) {
-          error.createDialog("Error finding file to read, must input valid file");
-          error.setVisible(true);
           System.out.println("Error finding file to read, must enter valid file path");
           System.exit(0);
         }
@@ -59,8 +54,6 @@ public class EasyAnimator {
         // parse the view type
         String viewType = args[Arrays.asList(args).indexOf(s) + 1];
         if (viewType.equals("")) {
-          error.createDialog("A view type is required for the program to run");
-          error.setVisible(true);
           System.out.println("A view type is required for the program to run");
           System.exit(0);
         }
@@ -86,63 +79,25 @@ public class EasyAnimator {
         speed = Integer.parseInt(args[Arrays.asList(args).indexOf(s) + 1]);
       }
     }
+
     // Make a model, make a view
     IModel model = new ModelImpl();
     IView view;
-    AnimationBuilder builder = new AnimationBuilderImpl(model);
+    Controller controller;
+    AnimationBuilder<IModel> builder = new AnimationBuilderImpl(model);
     // read file from CLI
     Readable inFile = new FileReader(infile);
     AnimationReader.parseFile(inFile, builder);
     // call view factory
     view = ViewFactory.makeView(type, speed, model);
-    switch (view.getViewType()) {
-      case TEXT:
-        System.out.println(view.getModelString(model));
-        break;
-      case SVG:
-        view.saveFile(view.getModelString(model), outfile);
-        break;
-      case GRAPHICAL:
-        drawAnimation((GraphicalView) view, speed);
-        break;
-      default:
-        System.out.println("Error determining the type of view.");
-        System.exit(0);
-        break;
-    }
+    // set up the controller
+    controller = new Controller(model, view);
+    controller.startAnimation(infile, type, outfile, speed);
+
     // TODO: Potential bugs...
     /*
     - buildings.txt doesn't work????
     - Writing to svg file working but kind of not working for some of the text inputs
-    - make JAR file
-    - Lastly, fix any broken tests
      */
-
-  }
-
-  /**
-   * Method to draw the actual image onto the canvas.
-   *
-   * @param tempo int tempo.
-   */
-  public static void drawAnimation(GraphicalView view, int tempo) {
-    view.setVisible(true);
-    int delay = 1000 / tempo;
-    ActionListener al =
-        new ActionListener() {
-          private int tick = 0;
-
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            view.atTick(tick);
-            tick++;
-            view.pack();
-            view.setResizable(true);
-            view.setMinimumSize(new Dimension(800, 900));
-            view.update(view.getGraphics());
-            view.repaint();
-          }
-        };
-    new Timer(delay, al).start();
   }
 }
---
+++ src/cs5004/mvc/controller/Controller.java
@@ -0,0 +1,82 @@
+package cs5004.mvc.controller;
+
+import cs5004.mvc.model.IModel;
+import cs5004.mvc.view.GraphicalView;
+import cs5004.mvc.view.IView;
+import cs5004.mvc.view.TypeOfView;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.Timer;
+
+/**
+ * Class to store the controller and to use this controller to handle the communication between
+ * model and the view.
+ */
+public class Controller {
+  private IModel model;
+  private IView view;
+
+  public Controller(IModel model, IView view) {
+    if (model == null || view == null) {
+      throw new IllegalArgumentException("Model or View are null");
+    }
+
+    this.model = model;
+    this.view = view;
+  }
+
+  /**
+   * Method that takes all the input and then starts the animation.
+   *
+   * @param infile file to read from.
+   * @param type type of view to process.
+   * @param outfile file to save to if specified.
+   * @param speed speed of the animation.
+   * @throws IOException if an error occurs with finding/writing to a file.
+   */
+  public void startAnimation(String infile, TypeOfView type, String outfile, int speed)
+      throws IOException {
+    switch (view.getViewType()) {
+      case TEXT:
+        System.out.println(view.getModelString(model));
+        break;
+      case SVG:
+        view.saveFile(view.getModelString(model), outfile);
+        break;
+      case GRAPHICAL:
+        drawAnimation((GraphicalView) view, speed);
+        break;
+      default:
+        System.out.println("Error determining the type of view.");
+        System.exit(0);
+        break;
+    }
+  }
+  /**
+   * Method to draw the actual image onto the canvas.
+   *
+   * @param tempo int tempo.
+   */
+  public void drawAnimation(GraphicalView view, int tempo) {
+    view.setVisible(true);
+    int delay = 1000 / tempo;
+    ActionListener al =
+        new ActionListener() {
+          private int tick = 0;
+
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            view.atTick(tick);
+            tick++;
+            view.pack();
+            view.setResizable(true);
+            view.setMinimumSize(new Dimension(800, 900));
+            view.update(view.getGraphics());
+            view.repaint();
+          }
+        };
+    new Timer(delay, al).start();
+  }
+}
