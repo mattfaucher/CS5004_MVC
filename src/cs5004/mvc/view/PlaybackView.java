c2dd1ebb5babd9638090d273e978ad5c35c48ce8
Matt Faucher <mfauch4444@gmail.com>
2021-04-23 11:04:28 -0400

    Added speed changing button

--- src/cs5004/mvc/view/PlaybackView.java
+++ src/cs5004/mvc/view/PlaybackView.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.Timer;
 
 public class PlaybackView extends AbstractView implements IView, ActionListener {
@@ -23,10 +24,14 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
   private JButton play;
   private JButton restart;
   private JCheckBox setLoop;
+  private JTextField setSpeed;
+  private JButton changeSpeed;
   private boolean paused;
   private boolean loop;
   private Timer timer;
   int tick = 0;
+  int speed;
+  int delay;
 
   /**
    * Constructs a new AbstractView Object.
@@ -42,6 +47,8 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.paused = false;
     this.loop = false;
+    this.speed = tempo;
+    this.delay = 1000 / speed;
 
     // set up main panel to house all other panels
     mainPanel = new JPanel();
@@ -49,19 +56,15 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
     mainPanel.setBackground(Color.white);
     mainPanel.setBorder(BorderFactory.createTitledBorder("Main Panel"));
     mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-    mainPanel.setDoubleBuffered(true);
 
     // set up drawPanel
     drawPanel = new DrawPanel(model);
     drawPanel.setBorder(BorderFactory.createTitledBorder("Animation Visualization"));
-    drawPanel.setVisible(true);
-    drawPanel.setDoubleBuffered(true);
 
     // set up data panel
     dataPanel = new JPanel();
     dataPanel.setBorder(BorderFactory.createTitledBorder("Data Output"));
     dataPanel.setBackground(Color.PINK);
-    dataPanel.setVisible(true);
     dataPanel.setLayout(new GridLayout());
 
     // set up the controlPanel
@@ -70,9 +73,8 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
     controlPanel.setLocation(50, 725);
     controlPanel.setSize(700, 130);
     controlPanel.setBackground(Color.gray);
-    controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.X_AXIS));
-    controlPanel.setVisible(true);
-    controlPanel.setDoubleBuffered(true);
+    controlPanel.setLayout(new GridLayout());
+    //controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.X_AXIS));
 
     // set up buttons
     play = new JButton();
@@ -85,12 +87,19 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
     setLoop.setText("Loop");
     setLoop.setSelected(false);
     setLoop.setActionCommand("loop");
+    setSpeed = new JTextField(this.speed);
+    setSpeed.setBorder(BorderFactory.createTitledBorder("Speed Input"));
+    changeSpeed = new JButton("Set Speed");
+    changeSpeed.setActionCommand("speed");
+
     // set listeners
     this.setListeners(this);
 
     controlPanel.add(play);
     controlPanel.add(restart);
     controlPanel.add(setLoop);
+    controlPanel.add(changeSpeed);
+    controlPanel.add(setSpeed);
 
     // add all to mainpanel
     mainPanel.add(drawPanel);
@@ -123,9 +132,9 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
    */
   private void restartButton(ActionEvent e) {
     timer.stop();
+    super.repaint();
     this.tick = 0;
-    atTick(this.tick);
-    timer.start();
+    render(this.speed);
   }
 
   /**
@@ -143,34 +152,37 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
     }
   }
 
-  @Override
-  public void render(int speed) {
-    setVisible(true);
-    int delay = 1000 / speed;
-    ActionListener al =
-        new ActionListener() {
-
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            if (timer.isRunning()) {
-              System.out.println(tick);
-              atTick(tick);
-              tick++;
-              setPreferredSize(new Dimension(1920, 1000));
-              pack();
-              update(getGraphics());
-            }
-          }
-        };
-    this.timer = new Timer(delay, al);
-    timer.start();
+  /**
+   * Method to handle the changing of the animation speed, dynamically.
+   *
+   * @param e ActionEvent.
+   */
+  private void speedToggle(ActionEvent e) {
+    String text = setSpeed.getText();
+    int val = Integer.parseInt(text);
+    if (val > 0) {
+      this.speed = Integer.parseInt(text);
+      this.delay = 1000 / this.speed;
+      System.out.println(this.speed);
+      render(this.speed);
+    }
   }
 
   @Override
-  public void setListeners(ActionListener click) {
-    this.play.addActionListener(click);
-    this.restart.addActionListener(click);
-    this.setLoop.addActionListener(click);
+  public void render(int speed) {
+    ActionListener al =
+        e -> {
+          if (timer.isRunning()) {
+            atTick(tick);
+            tick++;
+            setPreferredSize(new Dimension(1920, 1000));
+            pack();
+            update(getGraphics());
+            repaint();
+          }
+        };
+    this.timer = new Timer(this.delay, al);
+    timer.start();
   }
 
   @Override
@@ -183,23 +195,28 @@ public class PlaybackView extends AbstractView implements IView, ActionListener 
         restartButton(e);
         break;
       case "loop":
-        if (loop) {
-          loop = false;
-        } else {
-          loop = true;
-        }
+        loop = !loop;
         loopButton(e);
         break;
+      case "speed":
+        speedToggle(e);
       default:
         break;
     }
-    repaint();
   }
 
   public void atTick(int tick) {
     drawPanel.setTick(tick);
     drawPanel.paintComponent(getGraphics());
     drawPanel.revalidate();
+  }
+
+  @Override
+  public void setListeners(ActionListener click) {
+    this.play.addActionListener(click);
+    this.restart.addActionListener(click);
+    this.setLoop.addActionListener(click);
+    this.changeSpeed.addActionListener(click);
   }
 
   @Override
